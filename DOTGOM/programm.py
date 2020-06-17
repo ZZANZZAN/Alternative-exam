0febd18d9ea329c3f595f5275518190ea6681856
ZZANZZAN <40537652+ZZANZZAN@users.noreply.github.com>
2020-06-17 15:17:34 +0300

    Update programm.py

--- DOTGOM/programm.py
+++ DOTGOM/programm.py
@@ -10,67 +10,49 @@ import numpy as np
 import os
 
 sc = StandardScaler()
-len_spectr = 250
+len_spectr = 250 #длина спектра
 data = pd.read_csv(os.getcwd() + "\music_library.csv", sep = ';')
-X = data.iloc[:, 0:len_spectr].values
-y = data.iloc[:, 255].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=50)
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-regressor = RandomForestClassifier(n_estimators=200, random_state=50)
-regressor.fit(X_train, y_train)
-y_pred = regressor.predict(X_test)
+X = data.iloc[:, 0:len_spectr].values #спектры взятые из файла
+y = data.iloc[:, 255].values 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=50) #распределение полученных данных на тестовые и тренировочные
+X_train = sc.fit_transform(X_train) #подготовка входных данных для обучения случайного леса
+regressor = RandomForestClassifier(n_estimators=200, random_state=1) #создание случайного леса
+regressor.fit(X_train, y_train) #обучение случайного леса
 
-def getText():
+def Audio_processing_and_output():
     s = text.get(1.0, END)
     s = s[0: len(s)-1]
-    
     key = True
-    len_part = 390000
-    music_sig, samplerate = sf.read(os.getcwd() + "\\" + s)
-
-    for i_on_sig in range (0, int(len(music_sig)/len_part)):
+    len_part = 390000 #число, которое в итоге позволяет взять примерно равный 10 секундам отрывок
+    music_sig, samplerate = sf.read(os.getcwd() + "\\" + s) #поиск входного файла в дериктории где лежит программа и получение информации о файле
+    for i_on_sig in range (0, int(len(music_sig)/len_part)): #цикл где программа делит входной файл на примерно равные части в 10 сек
         sig = music_sig[i_on_sig*len_part:(i_on_sig+1)*len_part]
-
         N = int(len(sig)/1024)*1024
         spectrum_array = [0]*512
-
         for i in range(0, N-1, 1024):
             time_array = [0]*1024
             for j in range(0, 1023):
                 time_array[j] = sig[j+i][0]
-            spectrum = rfft(time_array, 1024)
-            spectrum_abc = np_abs(spectrum)
+            spectrum = rfft(time_array, 1024) #взятие БПФ из полученного отрывка
+            spectrum_abc = np_abs(spectrum) #взятие амплитудного спектра 
             for j in range(0, 511):
                 spectrum_array[j] += spectrum_abc[j]
-
         for j in range(0, 511):
             spectrum_array[j] = spectrum_array[j]/int(len(sig)/1024)
-
         y = np.array(spectrum_array)/N
-
         if key == False:
             a += [y[0:len_spectr]]
-
         if key == True: 
             a = [y[0:len_spectr]]
             key = False
-
-    
-    Test = a[0:len_spectr]
-    
-    Test = sc.transform(Test)
-
-    
+    Test = a[0:len_spectr]   
+    Test = sc.transform(Test) #подготовка полученных данных к прогонке через случайный лес    
     y_pred_test = regressor.predict(Test)
-
-    print(y_pred_test)
 
     arr = y_pred_test
     num = arr[0]
     N = len(arr)
     max_frq = 1
-
     for i in range(N-1):
         frq = 1
         for k in range(i+1,N):
@@ -103,7 +85,7 @@ label.pack()
 frame = Frame()
 frame.pack()
  
-b_get = Button(frame, text="Определить жанр", command=getText)
+b_get = Button(frame, text="Определить жанр", command=Audio_processing_and_output)
 b_get.pack(side=LEFT)
  
 b_delete = Button(frame, text="Удалить", command=deleteText)
